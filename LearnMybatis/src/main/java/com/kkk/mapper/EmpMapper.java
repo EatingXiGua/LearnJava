459d55e60623fa7f5bda9ce8f767063c9fa87c23
xvke1 <xvke0127@163.com>
2025-09-02 15:51:34 +0800

    mybatis基本使用-#{}和${}的区别

--- LearnMybatis/src/main/java/com/kkk/mapper/EmpMapper.java
+++ LearnMybatis/src/main/java/com/kkk/mapper/EmpMapper.java
@@ -6,4 +6,8 @@ import java.util.List;
 
 public interface EmpMapper {
     List<Emp> list();
+
+    //根据姓名查询
+    Emp getEmpByName(String name);
+    Emp getEmpByName2(String name);
 }
--- LearnMybatis/src/test/java/com/kkk/EmpTest.java
+++ LearnMybatis/src/test/java/com/kkk/EmpTest.java
@@ -1,10 +1,12 @@
 package com.kkk;
 
 import com.kkk.mapper.EmpMapper;
+import com.kkk.pojo.Emp;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -12,13 +14,25 @@ import java.io.InputStream;
 
 public class EmpTest {
 
-    @Test
-    public void test() throws IOException {
+    private EmpMapper empMapper = null;
+    @BeforeEach
+    public void setUp() throws Exception {
         InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        EmpMapper empMapper = sqlSession.getMapper(EmpMapper.class);
-        empMapper.list().forEach(System.out::println);
+        empMapper = sqlSession.getMapper(EmpMapper.class);
+    }
 
+    @Test
+    public void test() throws IOException {
+        empMapper.list().forEach(System.out::println);
+    }
+
+    @Test
+    public void test1() throws IOException {
+        Emp tom = empMapper.getEmpByName("tom");
+        System.out.println(tom);
+        Emp tom2 = empMapper.getEmpByName2("tom");
+        System.out.println(tom2);
     }
 }
