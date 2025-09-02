88a4d92e1375e697383997f658d36ddb1e1d2b17
xvke1 <xvke0127@163.com>
2025-09-02 20:11:48 +0800

    mybatis基本使用-单表操作，不同类型的数据的输入和输出

--- LearnMybatis/src/main/java/com/kkk/mapper/EmpMapper.java
+++ LearnMybatis/src/main/java/com/kkk/mapper/EmpMapper.java
@@ -1,8 +1,10 @@
 package com.kkk.mapper;
 
 import com.kkk.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface EmpMapper {
     List<Emp> list();
@@ -10,4 +12,39 @@ public interface EmpMapper {
     //根据姓名查询
     Emp getEmpByName(String name);
     Emp getEmpByName2(String name);
+
+    //数据输入
+    //1.单个简单类型参数
+    void deleteById(Integer empId);
+
+    //2.实体类类型参数
+    void addEmp(Emp emp);
+
+    //3.零散的简单类型数据
+    List<Emp> getEmpList(@Param("empName") String empName, @Param("minSalary") Double minSalary);
+
+    //4.Map类型的参数
+    void addEmp2(Map map);
+
+    //5.更新
+    void updateEmp(Emp emp);
+
+    //数据输入
+    //1.单个简单类型
+    Double getEmpSalary(Integer empId);
+
+    //2.返回实体类对象
+    Emp getEmpById(Integer empId);
+
+    //3.返回List
+    List<Double> getEmpSalaryList();
+
+    //4.返回Map
+    Map<String,Double> getMinMaxAvgSalary();
+
+    //5.返回主键值
+    void addEmp3(Emp emp);
+
+    //6.实体类属性和数据库字段对应关系
+    Emp getEmpById2(Integer empId);
 }
--- LearnMybatis/src/test/java/com/kkk/EmpTest.java
+++ LearnMybatis/src/test/java/com/kkk/EmpTest.java
@@ -11,28 +11,107 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
 
 public class EmpTest {
 
     private EmpMapper empMapper = null;
+    SqlSession sqlSession = null;
     @BeforeEach
     public void setUp() throws Exception {
         InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession sqlSession = sqlSessionFactory.openSession();
+        sqlSession = sqlSessionFactory.openSession();
         empMapper = sqlSession.getMapper(EmpMapper.class);
     }
 
     @Test
-    public void test() throws IOException {
+    public void test()   {
         empMapper.list().forEach(System.out::println);
     }
 
     @Test
-    public void test1() throws IOException {
+    public void test1()  {
         Emp tom = empMapper.getEmpByName("tom");
         System.out.println(tom);
         Emp tom2 = empMapper.getEmpByName2("tom");
         System.out.println(tom2);
+    }
+
+    @Test
+    public void test2() {
+        empMapper.deleteById(100);
+    }
+
+    @Test
+    public void test3() {
+        Emp lili = new Emp();
+        lili.setEmpName("lili");
+        lili.setEmpSalary(123.0);
+        empMapper.addEmp(lili);
+        sqlSession.commit();
+    }
+
+    @Test
+    public void test4() {
+        List<Emp> empList = empMapper.getEmpList("j", 5.0);
+        for (Emp emp : empList) {
+            System.out.println(emp);
+        }
+    }
+
+    @Test
+    public void test5() {
+        Map map = Map.of("empName","zhangsan","empSalary",123.0);
+        empMapper.addEmp2(map);
+        sqlSession.commit();
+    }
+
+    @Test
+    public void test6() {
+        Double empSalary = empMapper.getEmpSalary(1);
+        System.out.println(empSalary);
+    }
+
+    @Test
+    public void test7() {
+        Emp emp = empMapper.getEmpById(1);
+        System.out.println(emp);
+    }
+
+    @Test
+    public void test8() {
+        empMapper.getEmpSalaryList().forEach(System.out::println);
+    }
+
+    @Test
+    public void test9() {
+        Map<String, Double> minMaxAvgSalary = empMapper.getMinMaxAvgSalary();
+        minMaxAvgSalary.entrySet().forEach(System.out::println);
+    }
+
+    @Test
+    public void test10() {
+        Emp emp = new Emp();
+        emp.setEmpName("lisi");
+        emp.setEmpSalary(4000.0);
+        empMapper.addEmp3(emp);
+        System.out.println(emp);
+        sqlSession.commit();
+    }
+
+    @Test
+    public void test11() {
+        Emp emp = empMapper.getEmpById2(1);
+        System.out.println(emp);
+    }
+
+    @Test
+    public void test12() {
+        Emp emp = empMapper.getEmpById2(1);
+        emp.setEmpSalary(500.21);
+        empMapper.updateEmp(emp);
+        sqlSession.commit();
     }
 }
